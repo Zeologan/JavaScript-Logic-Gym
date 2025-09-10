c1b4a3589450ee6f7d11f32a8453d407776c355a
unknown <rohitratate855@gmail.com>
2025-09-10 10:50:34 +0530

    Day 18 Completed

---
+++ Day 18/app.js
@@ -0,0 +1,7 @@
+import { add, multiply, pi } from "./mathUtils.js";
+
+console.log("Add : ", add(1,2));
+console.log("Add : ", add(3,5));
+console.log("Multiply : ", multiply(5,9));
+console.log("Multiply : ", multiply(0,100));
+console.log("Multipy : ", multiply(pi,20));
---
+++ Day 18/user.js
@@ -0,0 +1,6 @@
+const user = {
+    name : "sam",
+    age : 23,
+};
+
+export default user
