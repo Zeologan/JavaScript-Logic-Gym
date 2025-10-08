6779d8744be81b3b886af2bf9d08cc312702511b
unknown <rohitratate855@gmail.com>
2025-10-08 10:04:06 +0530

    Day 27 completed

--- Day 27/dataObject.js
+++ Day 27/dataObject.js
@@ -1,9 +1,32 @@
 const date = new Date();
-const today = new Date();
-console.log(date)
-console.log(date.getFullYear());
-console.log(date.getMonth())
-console.log(date.getDay())
-console.log(date.getTime())
-console.log(today.toLocaleDateString());  
-console.log(today.toLocaleString());  
+// const today = new Date();
+// console.log(date)
+// console.log(date.getFullYear());
+// console.log(date.getMonth())
+// console.log(date.getDay())
+// console.log(date.getTime())
+// console.log(today.toLocaleDateString());  
+// console.log(today.toLocaleString());  
+// console.log(today.toLocaleDateString("en-IN", {
+//   weekday: "long",
+//   year: "numeric",
+//   month: "numeric",
+//   day: "numeric"
+// }));
+
+// Simple Timer 
+const target = new Date("Oct 21, 2025 00:00:00").getTime();
+const timer = setInterval(() => {
+    const now = new Date().getTime();
+    const diff = target - now;
+
+    // Convert milliseconds to days, hours, minutes, seconds
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    console.log(`${days}days left for Diwali!`);
+
+    if(diff < 0){
+        clearInterval(timer);
+        console.log("Happy Diwali");
+    }
+}, 1000);
